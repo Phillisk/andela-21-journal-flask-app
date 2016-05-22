ad9add65ac85ced01cd4f3f6ed49d5a57b18d15f
Phillis Kiragu <kiragu.w.phillis@gmail.com>
2016-05-22 22:26:43 +0300

    add flask login

--- app/main/models.py
+++ app/main/models.py
@@ -1,6 +1,13 @@
-from app import db, app
+from app import db, lm, app
 from werkzeug import generate_password_hash, check_password_hash
+from flask.ext.login import UserMixin
 
+import sys
+if sys.version_info >= (3, 0):
+    enable_search = False
+else:
+    enable_search = True
+    import flask.ext.whooshalchemy as whooshalchemy
 
 
 # Define a base model for other database tables to inherit
@@ -13,7 +20,7 @@ class Base(db.Model):
 
 
 # Define a User model
-class User(Base):
+class User(UserMixin, Base):
 
     __tablename__ = 'user'
 
@@ -23,7 +30,8 @@ class User(Base):
     username = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(128), nullable=False, unique=True)
     password = db.Column(db.String(192), nullable=False)
-    
+    #journals = db.relationship("Journal", backref='author', lazy='dynamic', primaryjoin="User.id == Journal.user_id")
+
     # New instance instantiation procedure
     def __init__(self, firstname, lastname, username, email, password):
 
@@ -32,6 +40,24 @@ class User(Base):
         self.username = username
         self.email = email
         self.set_password(password)
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2
+        except NameError:
+            return str(self.id)  # python 3
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -44,11 +70,24 @@ class User(Base):
     def check_password(self, password_string):
         return check_password_hash(self.password, password_string)
 
+@lm.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class Tag(Base):
+    """ This model will create a table for tags"""
+    __tablename__ = 'tags'
+    id = db.Column(db.Integer)
+    tagname = db.Column(db.String(128), primary_key=True )
+
 
 class Journal(Base):
     """this is the  model for journals """
     __tablename__ = 'journal'
-    
+    __searchable__ = ['body', 'title', 'tags']
+
+
     # journal details
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
@@ -67,5 +106,7 @@ class Journal(Base):
         return '<Journal %r>' % self.title
 
 
+if enable_search:
+        whooshalchemy.whoosh_index(app, Journal)
 
 
