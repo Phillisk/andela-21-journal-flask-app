f494848ad5b86ff6a5c0f12e2edb7b0caf09ac6f
Phillis Kiragu <kiragu.w.phillis@gmail.com>
2016-06-01 13:20:25 +0300

    add flask  login requirements

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,13 @@ from flask import Flask, render_template
 # Import extensions e.g. SQLAlchemy, flask-restful
 #from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask_restful import Api
+from flask.ext.login import LoginManager
+#from flask_restful import Api
 
 # Define the WSGI application object and api
 app = Flask(__name__)
 
-api = Api(mod_main)
+#api = Api(mod_main)
 
 
 # Configurations
@@ -19,6 +20,10 @@ app.config.from_object('config')
 # by modules and controllers and flask_migrate for migrations
 db = SQLAlchemy(app)
 
+#settings for flask login
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'auth.login'
 
 # Sample HTTP error handling
 @app.errorhandler(404)
@@ -27,10 +32,13 @@ def not_found(error):
 
 # Import a module / component using its blueprint handler variable (mod_auth)
 
-from app.main.api import main as main_module
+from app.main.views import mod_main as main_module
+from app.auth.views import mod_auth as auth_module
+
 
 # Register blueprint(s)
-app.register_blueprint(mod_main)
+app.register_blueprint(main_module)
+app.register_blueprint(auth_module)
 #api.add_resource()
 # Build the database:
 # This will create the database file using SQLAlchemy
